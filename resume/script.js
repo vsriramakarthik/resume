a437927d02a1208a3a255b89c32519664f0d92e0
vsriramakarthik <147160205+vsriramakarthik@users.noreply.github.com>
2024-12-27 23:35:50 -0500

    Delete resume directory

--- resume/script.js
+++
@@ -1,9 +0,0 @@
-// Example: Toggle visibility of sections
-document.querySelectorAll("h3").forEach((sectionTitle) => {
-    sectionTitle.addEventListener("click", () => {
-      const sectionContent = sectionTitle.nextElementSibling;
-      sectionContent.style.display =
-        sectionContent.style.display === "none" ? "block" : "none";
-    });
-  });
-  
